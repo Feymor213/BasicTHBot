19cb3cc615907321cdc29395ee22272ca8b4bce6
Feymor213 <none>
2025-01-31 20:11:49 +0000

    Додані базові приклади використання мови

---
+++ examples/basics.py
@@ -0,0 +1,51 @@
+
+# Змінні
+x = "Hello world!"
+
+# Текст (string)
+text = "Hello world"
+# Ціле число (integer)
+x = 13
+# Неціле число (float)
+y = 1.4
+# Логічне значення (bool)
+value = True
+value = False
+
+# Списки
+lst = [1, 5, 3, 7, 0, 3, 7, 14, 52]
+lst[0] # 1;
+lst[1] # 5
+lst[7] # 14
+# Довжина
+len(lst) # 9
+
+# До слів можна відноситись як до списків
+string = "Hello world!"
+string[0] # H
+string[6] # w
+len(string) # 12
+
+
+# Умовні оператори if, else
+variable = 0
+if variable == 1:
+    print("Одиниця")
+else:
+    print("Не одиниця")
+
+# Оператори порівняння ==, !=, <, >, =<, =>
+a = "Hello world"
+a == "Hello world" # True/False
+
+b = a == 'Hello world' # True
+
+
+
+
+
+
+
+
+
+
